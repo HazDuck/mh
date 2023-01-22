6225d04995a9e3d39065cd8b42727e350f857964
Pete Heyes <peter.e.heyes@gmail.com>
2023-01-22 21:13:25 +0000

    Get account data via fetch rather than hardcoded

--- pages/index.js
+++ pages/index.js
@@ -1,11 +1,23 @@
 import Detail from "../modules/property-details";
 import {Banner} from "../components/banner"
 
-export default function PropertyDetails() {
+export const getStaticProps = async () => {
+  const res = await fetch('http://localhost:3333/api/account')
+  if (res?.ok) {
+    const data  = await res.json()
+    return {
+      props: {accountData: data}
+    }
+  } else {
+    throw new Error('Something went wrong');
+  }
+}
+
+export default function PropertyDetails({accountData}) {
   return (
     <>
       <Banner>Property Details</Banner>
-      <Detail/>
+      <Detail accountData={accountData}/>
     </>   
   );
 }
